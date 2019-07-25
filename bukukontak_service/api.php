0754c3d3c674f780f31d97867f65e197c9e7c111
Ardan Anjung Kusuma <ardananjungkusuma@gmail.com>
2019-07-25 09:36:19 +0700

    files for imageApi
    place in htdocs man

---
+++ bukukontak_service/api.php
@@ -0,0 +1,116 @@
+	<?php
+class BukuKontak_Service
+{
+	const ENDPOINT = '/bukukontak_service/api.php/';
+	
+	private $_resource;
+	private $_method;
+	private $_params;
+	private $_handler;
+	
+	// Database connection
+	private $_db;
+	
+	public function __construct()
+	{
+		$this->_resource = str_replace(self::ENDPOINT, '', $_SERVER['REQUEST_URI']);
+		
+		if(strpos($this->_resource, '/') !== false)
+		{
+			$explode = explode('/', $this->_resource);
+			
+			$this->_resource = $explode[0];
+			
+			$this->_params = array_slice($explode, 1);
+		}
+		
+		$this->_method   = $_SERVER['REQUEST_METHOD'];
+		
+		$this->_handler = strtolower($this->_method . '_' . $this->_resource);
+		
+		$this->_initDb();
+	}
+	
+	private function _initDb()
+	{
+		$this->_db = new mysqli('localhost', 'root', '', 'bukukontak_service');
+		// Check connection
+		if ($this->_db->connect_error)
+		    die("Connection failed: " . $conn->connect_error);
+	}
+	
+	private function _readFromDb($sql)
+	{
+		$query = $this->_db->query($sql);
+		
+		$results = array();
+		
+		while($row = $query->fetch_assoc())
+			$results[] = $row;
+		
+		return $results;
+	}
+	
+	private function _writeToDb($sql)
+	{
+		if($this->_db->query($sql) === true)
+			return true;
+		
+		return false;
+	}
+	
+	public function serve()
+	{
+		$func = $this->_handler;
+		
+		$this->$func($this->_params);
+	}
+	
+	public static function main()
+	{
+		$service = new BukuKontak_Service();
+		
+		$service->serve();
+	}
+	
+	private function get_contacts($params)
+	{
+		
+		$contacts = $this->_readFromDb("SELECT * FROM contact");
+		
+		echo json_encode($contacts);
+		
+	}
+	
+	
+	private function post_contacts($params)
+	{
+		// Harus pakai ini jika content-type-nya Application/Json
+		$rawContent = trim(file_get_contents("php://input"));
+		
+		// Parse jadi array
+		$data = json_decode($rawContent, true);
+			
+		$id          = $data['id'];
+		$name        = $data['name'];
+		$phoneNumber = $data['phone_number'];
+		$image		 = $data['image'];
+		
+		$inserted = $this->_writeToDb("INSERT INTO contact (name, phone_number,image) VALUES ('$name', '$phoneNumber','$image')");
+		
+		if($inserted)
+		{
+			$insertedContact = array(
+				'id' => $id,
+				'name' => $name,
+				'phone_number' => $phoneNumber,
+				'image' => $image
+			);
+		
+			echo json_encode($insertedContact);
+		}
+	}
+}
+BukuKontak_Service::main();
+	
+?>
